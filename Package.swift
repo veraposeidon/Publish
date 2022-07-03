233215dce1e05db93960be6aa89e0ff65c795774
veraposeidon <veraposeidon@gmail.com>
2022-07-03 19:58:47 +0800

    feat: support Ink 0.5.2

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     dependencies: [
         .package(
             name: "Ink",
-            url: "https://github.com/johnsundell/ink.git",
-            from: "0.2.0"
+            url: "https://github.com/veraposeidon/ink.git",
+            from: "0.5.2"
         ),
         .package(
             name: "Plot",
